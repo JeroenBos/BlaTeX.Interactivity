86391ac2d04992ab40eedb96ac89f52ece910f45
Jeroen Bos <jer123bos@gmail.com>
2021-08-15 17:00:55 +0200

    Check if it was downloaded

--- .github/workflows/Download_LayoutEngine.sh
+++ .github/workflows/Download_LayoutEngine.sh
@@ -7,3 +7,5 @@ case "${unameOut}" in
     MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe ;;
     *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine         ;;
 esac
+cd tools
+ls
