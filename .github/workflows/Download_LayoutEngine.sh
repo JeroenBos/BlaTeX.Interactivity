afd2cd81f22ee5241b56a2774c24a54dce6030c1
Jeroen Bos <jer123bos@gmail.com>
2021-08-15 20:52:32 +0200

    Print LayoutEngine version in CI

--- .github/workflows/Download_LayoutEngine.sh
+++ .github/workflows/Download_LayoutEngine.sh
@@ -4,9 +4,12 @@
 mkdir -p tools
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe ;;
+    MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe;
+                tools/LayoutEngine.exe --version
+                ;;
     *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine;
-                sudo chmod 755 './tools/layoutengine'                                                                                                 ;;
+                sudo chmod 755 './tools/layoutengine';
+                tools/LayoutEngine --version;
+                ;;
 esac
-cd tools
-ls
+
