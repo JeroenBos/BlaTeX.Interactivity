274139da6b381359c1335eac6374efdd4421c1db
Jeroen Bos <jer123bos@gmail.com>
2021-08-15 16:52:22 +0200

    Download LayoutEngine in CI

---
+++ .github/workflows/Download_LayoutEngine.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# alternatively I could set up a submodule...
+mkdir -p tools
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe ;;
+    *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine         ;;
+esac
