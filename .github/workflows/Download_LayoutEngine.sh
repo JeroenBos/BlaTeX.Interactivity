05b89b6dbda845a4f5ee61e998e30cf3cf774389
Jeroen Bos <jer123bos@gmail.com>
2021-08-15 17:41:55 +0200

    Make executable executable

--- .github/workflows/Download_LayoutEngine.sh
+++ .github/workflows/Download_LayoutEngine.sh
@@ -5,7 +5,8 @@ mkdir -p tools
 unameOut="$(uname -s)"
 case "${unameOut}" in
     MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe ;;
-    *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine         ;;
+    *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine;
+                sudo chmod 755 './tools/layoutengine'                                                                                                 ;;
 esac
 cd tools
 ls
