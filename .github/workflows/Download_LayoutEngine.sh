49b7924ffaf946486e6c24c21f5a86a6cda3b2ee
Jeroen Bos <jer123bos@gmail.com>
2021-10-25 22:06:02 +0200

    Fix typo

--- .github/workflows/Download_LayoutEngine.sh
+++ .github/workflows/Download_LayoutEngine.sh
@@ -5,12 +5,12 @@ mkdir -p tools
 unameOut="$(uname -s)"
 case "${unameOut}" in
     MINGW*)     curl -L -o tools/LayoutEngine.exe https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine.exe;
-                rm tools/chromdriver.exe;
+                rm tools/chromedriver.exe -f;
                 tools/LayoutEngine.exe --version;
                 ;;
     *)          curl -L -o tools/layoutengine https://github.com/JeroenBos/JBSnorro.LayoutEngine/raw/master/LayoutEngine/publish/LayoutEngine;
                 sudo chmod 755 './tools/layoutengine';
-                rm tools/chromdriver;
+                rm tools/chromedriver -f;
                 tools/LayoutEngine --version;
                 ;;
 esac
