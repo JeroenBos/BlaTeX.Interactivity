8219a8c7d52372a49fd4464a71bf79c096a2d9d9
Jeroen Bos <jer123bos@gmail.com>
2021-10-28 14:09:15 +0200

    Exclude svg styles from test

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -30,17 +30,28 @@ const getStyle = (value: number): string => {
             return 'fill:yellow;fill-rule: evenodd' + common;
     }
 };
+const getTestableSvgPart = (svg: string): string => {
+    while (true) {
+        const pattern = ' style="';
+        const styleIndex = svg.indexOf(pattern);
+        if (styleIndex < 0) break;
+
+        const closingQuote = svg.indexOf('"', styleIndex + pattern.length);
+        svg = svg.substr(0, styleIndex) + svg.substr(closingQuote + 1);
+    }
+    return svg;
+};
 describe('Color HTML based on source locations', () => {
     it('<div>TEXT</div>', async () => {
         const html = '<div>TEXT</div>';
         const element = await toHTMLWithRectangles(html);
         const svg = allPointsByIndexToSVGByProximity(element, getStyle);
 
-        console.log(svg);
+        const testableSvgPart = getTestableSvgPart(svg);
         assert(
-            svg ===
+            testableSvgPart ===
                 `<svg width="784" height="18">
-<path d="M792,0 0,0 0,26 792,26 792,0" style="fill:gray;fill-rule: evenodd" />
+<path d="M792,0 0,0 0,26 792,26 792,0" />
 </svg>`
         );
     });
@@ -50,12 +61,12 @@ describe('Color HTML based on source locations', () => {
         const element = await toHTMLWithRectangles(html);
         const svg = allPointsByIndexToSVGByProximity(element, getStyle);
 
-        console.log(svg);
+        const testableSvgPart = getTestableSvgPart(svg);
         assert(
-            svg ===
+            testableSvgPart ===
                 `<svg width="784" height="18">
-<path d="M392,0 0,0 0,26 392,26 392,0" style="fill:black;fill-rule: evenodd" />
-<path d="M792,26 792,0 400,0 400,0 392,0 392,26 792,26" style="fill:red;fill-rule: evenodd" />
+<path d="M392,0 0,0 0,26 392,26 392,0" />
+<path d="M792,26 792,0 400,0 400,0 392,0 392,26 792,26" />
 </svg>`
         );
     });
@@ -65,12 +76,12 @@ describe('Color HTML based on source locations', () => {
         const element = await toHTMLWithRectangles(html);
         const svg = allPointsByIndexToSVGByProximity(element, getStyle);
 
-        console.log(svg);
+        const testableSvgPart = getTestableSvgPart(svg);
         assert(
-            svg ===
+            testableSvgPart ===
                 `<svg width="784" height="36">
-<path d="M392,0 0,0 0,44 392,44 392,0" style="fill:black;fill-rule: evenodd" />
-<path d="M792,44 792,0 400,0 400,0 392,0 392,44 792,44" style="fill:red;fill-rule: evenodd" />
+<path d="M392,0 0,0 0,44 392,44 392,0" />
+<path d="M792,44 792,0 400,0 400,0 392,0 392,44 792,44" />
 </svg>`
         );
     });
@@ -80,15 +91,16 @@ describe('Color HTML based on source locations', () => {
         const element = await toHTMLWithRectangles(html);
         const svg = allPointsByIndexToSVGByProximity(element, getStyle);
 
+        const testableSvgPart = getTestableSvgPart(svg);
         assert(
-            svg ===
+            testableSvgPart ===
                 `<svg width="784" height="18">
-<path d="M21,0 0,0 0,26 21,26 21,0" style="fill:black;fill-rule: evenodd" />
+<path d="M21,0 0,0 0,26 21,26 21,0" />
 <path d="M62,26 106,26 106,0 62,0 62,26
  M106,26 204,26 204,0 106,0 106,26
  M204,26 400,26 400,0 204,0 204,26
- M400,26 792,26 792,0 400,0 400,26" style="fill:yellow;fill-rule: evenodd" />
-<path d="M62,0 21,0 21,26 62,26 62,0" style="fill:red;fill-rule: evenodd" />
+ M400,26 792,26 792,0 400,0 400,26" />
+<path d="M62,0 21,0 21,26 62,26 62,0" />
 </svg>`
         );
     });
