2dc16de9262d3f31713a600f658c48693490b6f0
Jeroen Bos <jer123bos@gmail.com>
2021-11-21 19:00:12 +0100

    TDD

--- test/pointer.spec.ts
+++ test/pointer.spec.ts
@@ -3,6 +3,12 @@ import {
     toHTMLElementWithBoundingRectanglesWithKatex,
 } from './jsdom.understanding.spec';
 import { getCursorIndexByProximity, getDistance_FOR_TESTING_ONLY } from '../src/PointToCursorHandleConverter';
+import fs from 'fs';
+import { overlayBodyWithKatexCSS } from './utils/overlay';
+import Point from '../src/polyfills/Point';
+import { assert } from '../src/utils';
+import { debug_it, getStyle } from './utils/utils';
+import { allPointsByIndexToSVGByProximity } from '../src/paintAllPointsByIndex';
 
 describe('Resolve location to parsetree location', () => {
     it('Simple <div> without annotations yields no location', async () => {
@@ -62,5 +68,31 @@ describe('Test getDistance internally.', () => {
         expect(distancesToOrigin.offsetToRight).toBe(-13.4375);
         expect(distancesToOrigin.offsetToTop).toBe(-1);
         expect(distancesToOrigin.offsetToBottom).toBe(-22);
+    });
+});
+
+describe('Test point to cursor handler for specific points.', () => {
+    debug_it('on x_1^2', async (zoom: boolean) => {
+        const htmlBody = fs.readFileSync('./test/AnnotatedData/x_1^2.html').toString();
+        const element = await toHTMLElementWithBoundingRectangles(htmlBody, true, zoom ? { zoom: 500 } : undefined);
+
+        overlayBodyWithKatexCSS(htmlBody, allPointsByIndexToSVGByProximity(element, getStyle)); // debug purposes only
+
+        const points = [
+            { p: new Point(4, 5), index: 0 },
+            { p: new Point(6, 10), index: 1 },
+            { p: new Point(13, 13), index: 4 },
+        ];
+
+        for (const { p, index } of points) {
+            const result = getCursorIndexByProximity(element, p);
+
+            // debugging purposes:
+            const svg = `<svg><rect x="${p.x}" y="${p.y}" width="1" height="1" style="fill: red"/></svg>`;
+            overlayBodyWithKatexCSS(htmlBody + `<div>${result}</div>`, svg, './test/x_1^2_after.html'); // debug purposes only
+
+            assert(result === index);
+            console.log(`Point (${p.x}, ${p.y}) successful`);
+        }
     });
 });
