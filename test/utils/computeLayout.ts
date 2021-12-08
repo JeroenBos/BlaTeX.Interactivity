b4ce696a78f7856b04297a7231fc91e03a0c9a2d
Jeroen Bos <jer123bos@gmail.com>
2021-12-08 22:44:53 +0100

    Improve

--- test/utils/computeLayout.ts
+++ test/utils/computeLayout.ts
@@ -123,7 +123,7 @@ async function _computeLayout(path: string, layoutConfig: LayoutConfig, tag: str
     writeNoteLine(`${tag}.process = ${startTime.ms}`);
     if (subprocess.error !== undefined) {
         // handle failure to start the process
-        if (subprocess.stderr.includes("ETIMEDOUT")) {
+        if (subprocess.error.message.includes(" ETIMEDOUT")) {
             throw new TimedoutException()
         }
         tcs.reject(subprocess.error);
