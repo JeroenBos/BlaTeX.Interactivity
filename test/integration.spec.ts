e1b43c802180287bf96252c027f872029c068644
Jeroen Bos <jer123bos@gmail.com>
2021-12-08 22:55:58 +0100

    Why did I get normal svgs this time?

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -16,7 +16,7 @@ import { HorizontalClosestDistanceType, MinDistances, VerticalClosestDistanceTyp
 import { initGlobalTypesFromJSDOM } from '.';
 
 function readExpectedSvg(name: string, zoom: boolean): string {
-    const contents = readAllText("./test/expected/" + name  + (zoom ? ".zoom" : isCI ? ".ci" : "") + ".svg");
+    const contents = readAllText("./test/expected/" + name  + (zoom ? ".zoom" : "") + ".svg");
     return contents.trim();
 }
 describe('Color HTML based on source locations', () => {
