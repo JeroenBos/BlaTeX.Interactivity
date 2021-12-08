658f00a67ae46de550c0e7c4fb1d4037d1f5dd68
Jeroen Bos <jer123bos@gmail.com>
2021-12-08 22:23:39 +0100

    Implement readAllText

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import fs from 'fs';
 export { getCursorIndexByProximity } from './PointToCursorHandleConverter';
+
+
+export function readAllText(path: string): string {
+    return fs.readFileSync(path).toString();
+}
